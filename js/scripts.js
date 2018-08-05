3a95577214aeb79f2882412baec1cf42c8136219
laskadrin <laskadrin@gmail.com>
2018-08-05 18:50:58 +0300

    both done

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,7 @@
 $('.dropdown-trigger').dropdown({
     coverTrigger: false
 });
-/*function dashboard() { 
-    document.getElementById("dashboard").style.display="flex";
-    document.getElementById("other").style.display="none";  
-    } 
-function other() { 
-    document.getElementById("dashboard").style.display="none"; 
-    document.getElementById("other").style.display="flex"; 
-}*/
+
 $( document ).ready(function() {
     $("#dashboard").toggle(false),
     $("#other").toggle(false),
